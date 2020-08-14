8ddef6e5abaff41aa58a90af9aca5a4abd1bff46
Vikram-bohra <56751660+Vikram-bohra@users.noreply.github.com>
2020-08-14 09:54:37 +0530

    Update settings.py

--- NAV_value_webscrapping/NAV_value/settings.py
+++ NAV_value_webscrapping/NAV_value/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'izcf9cydm3mo)k3-$&#-(7qgn)zb&5x3pq1-yu-$3i(^*mzb-#'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
