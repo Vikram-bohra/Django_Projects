b38583e10c57668e174d2eed6280b283b82a0b61
Vikram-bohra <56751660+Vikram-bohra@users.noreply.github.com>
2020-08-14 09:55:39 +0530

    Update settings.py

--- loan_prediction_ML_with_django/loan_pred/settings.py
+++ loan_prediction_ML_with_django/loan_pred/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4lgm8anyg#r_duxp^6)3-k!($=04tu*&+e-f7baguoqa352ddc'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
